1341e2742b92783397f246e808e53c9ba7ddeb80
Ilya N. Zykin <ilya.zykin@toptal.com>
2019-10-28 23:02:09 +0300

    Update migrations and schema

--- db/migrate/20120314061307_create_pages.rb
+++ db/migrate/20120314061307_create_pages.rb
@@ -1,4 +1,4 @@
-class CreatePages < ActiveRecord::Migration
+class CreatePages < ActiveRecord::Migration[5.2]
   def change
     create_table :pages do |t|
       t.integer :user_id
--- db/schema.rb
+++ db/schema.rb
@@ -10,46 +10,46 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20120314061307) do
+ActiveRecord::Schema.define(version: 2012_03_14_061307) do
 
   create_table "pages", force: :cascade do |t|
-    t.integer  "user_id"
-    t.integer  "person_id"
-    t.string   "title"
-    t.text     "content"
-    t.string   "state",      default: "draft"
-    t.datetime "created_at"
-    t.datetime "updated_at"
+    t.integer "user_id"
+    t.integer "person_id"
+    t.string "title"
+    t.text "content"
+    t.string "state", default: "draft"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
   end
 
   create_table "roles", force: :cascade do |t|
-    t.string   "name",        default: ""
-    t.string   "title",       default: ""
-    t.text     "description"
-    t.text     "the_role",                 null: false
-    t.datetime "created_at"
-    t.datetime "updated_at"
+    t.string "name", default: ""
+    t.string "title", default: ""
+    t.text "description"
+    t.text "the_role", null: false
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
   end
 
   create_table "users", force: :cascade do |t|
-    t.string   "name"
-    t.string   "company"
-    t.string   "address"
-    t.string   "some_protected_field",   default: "should_not_be_changed"
-    t.string   "email",                  default: "",                      null: false
-    t.string   "encrypted_password",     default: "",                      null: false
-    t.string   "password",               default: "",                      null: false
-    t.string   "reset_password_token"
+    t.string "name"
+    t.string "company"
+    t.string "address"
+    t.string "some_protected_field", default: "should_not_be_changed"
+    t.string "email", default: "", null: false
+    t.string "encrypted_password", default: "", null: false
+    t.string "password", default: "", null: false
+    t.string "reset_password_token"
     t.datetime "reset_password_sent_at"
-    t.integer  "role_id"
+    t.integer "role_id"
     t.datetime "remember_created_at"
-    t.integer  "sign_in_count",          default: 0
+    t.integer "sign_in_count", default: 0
     t.datetime "current_sign_in_at"
     t.datetime "last_sign_in_at"
-    t.string   "current_sign_in_ip"
-    t.string   "last_sign_in_ip"
-    t.datetime "created_at"
-    t.datetime "updated_at"
+    t.string "current_sign_in_ip"
+    t.string "last_sign_in_ip"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
     t.index ["email"], name: "index_users_on_email", unique: true
     t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
   end
